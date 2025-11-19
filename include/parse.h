86a1e61fdb85c05edd8b6fc1811b13f238a60d6b
Frasier <fsundra@gmail.com>
2025-11-19 15:04:09 +0800

    renaming to avoid conflicts with webhook

--- include/parse.h
+++ include/parse.h
@@ -28,8 +28,8 @@ struct employee_t {
 // int output_file(int fd, struct db_header_t *);
 
 int create_db_header(struct dbheader_t **);
-int validate_db_header(int fd, struct dbheader_t **headerOut);
-int read_employees(int fd, struct dbheader_t *, struct employee_t **employeesOut);
-int output_file(int fd, struct dbheader_t *, struct employee_t *employees);
+int validate_db_header(int fd, struct dbheader_t **);
+int read_employees(int fd, struct dbheader_t *, struct employee_t **);
+int output_file(int fd, struct dbheader_t *, struct employee_t *);
 
 #endif // !PARSE_H
